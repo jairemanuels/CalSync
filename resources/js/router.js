ed181b75eaa64409b3bb8b27e85b5c0666315c1b
Jens Gryspeert <jens@salesnote.eu>
2024-09-07 19:48:28 +0200

    Implement Vue3 and fix login

---
+++ resources/js/router.js
@@ -0,0 +1,8 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [];
+
+export default createRouter({
+    history: createWebHistory(),
+    routes,
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-        "./resources/**/*.vue",
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/laravel/jetstream/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.vue',
     ],
 
     theme: {
@@ -14,26 +17,8 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
-            colors: {
-                google: {
-                    'text-gray': '#3c4043',
-                    'button-blue': '#1a73e8',
-                    'button-blue-hover': '#5195ee',
-                    'button-dark': '#202124',
-                    'button-dark-hover': '#555658',
-                    'button-border-light': '#dadce0',
-                    'logo-blue': '#4285f4',
-                    'logo-green': '#34a853',
-                    'logo-yellow': '#fbbc05',
-                    'logo-red': '#ea4335',
-                },
-            },
         },
     },
-    plugins: [forms],
+
+    plugins: [forms, typography],
 };
-
-
-
-
-
