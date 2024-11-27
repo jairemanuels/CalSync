319fc9852a8012c5b2b619bb501b401691f441ab
Jaïr Emanuels <JAIR-EMANUELS@PROTONMAIL.COM>
2024-11-27 10:53:42 +0100

    All events + avatar

--- app/Http/Controllers/GoogleEventsController.php
+++ app/Http/Controllers/GoogleEventsController.php
@@ -16,42 +16,48 @@ class GoogleEventsController extends Controller
     {
         $user = auth()->user();
 
-
         $google_client_token = [
             'access_token' => $user->provider_token,
             'refresh_token' => $user->refresh_token,
             'expires_in' => $user->expires_in,
         ];
 
-
-
-        $client = new Client();
+        $client = new \Google\Client();
         $client->setApplicationName("Timely");
         $client->setDeveloperKey(config('services.google.client_id'));
         $client->setAccessToken(json_encode($google_client_token));
 
-        $service = new Calendar($client);
+        $service = new \Google\Service\Calendar($client);
 
-        $events = $service->events->listEvents($user->email);
+        $allEvents = []; // Opslag voor alle evenementen
+        $pageToken = null;
 
-        /** @var Event $event */
-        $events = $events->getItems();
+        do {
+            $events = $service->events->listEvents($user->email, [
+                'maxResults' => 250, // de norm van google
+                'pageToken' => $pageToken,
+                'singleEvents' => true, // Optioneel, afhankelijk van je usecase tbh
+                'orderBy' => 'startTime', // Sorteer op starttijd
+            ]);
 
-        foreach ($events as $event) {
-            if ($event->start?->getDateTime() && $event->end?->getDateTime()) {
-                if (! Event::where(['external_id' => $event->id])->exists()) {
-                    Event::updateOrCreate([
+            $allEvents = array_merge($allEvents, $events->getItems());
+            $pageToken = $events->getNextPageToken(); // Volgende pagina-token
+        } while ($pageToken);
+
+        foreach ($allEvents as $event) {
+            if ($event->start->getDateTime() && $event->end->getDateTime()) {
+                Event::updateOrCreate(
+                    ['external_id' => $event->id],
+                    [
                         'external_id' => $event->id,
                         'user_id' => $user->id,
                         'name' => $event->summary,
                         'description' => $event->description,
                         'event_time_start' => $event->start->getDateTime(),
                         'event_time_end' => $event->end->getDateTime(),
-                    ]);
-                }
+                    ]
+                );
             }
         }
-
-
     }
 }
